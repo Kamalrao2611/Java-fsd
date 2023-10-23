537b8a8f1a43c0304a08d494cdbf3cf6bd07605a
Kamal <kamalrao2611@gmail.com>
2023-10-23 22:51:21 +0530

    Changes have been committed.

---
+++ src/Typecasting.java
@@ -0,0 +1,33 @@
+
+public class Typecasting {
+
+	public static void main(String[] args) {
+		System.out.println("Implicit Type Casting");
+		
+		char x='K';
+		System.out.println("Value of a: "+x);
+		
+		int b=x;
+		System.out.println("Value of b: "+b);
+		
+		float c=x;
+		System.out.println("Value of c: "+c);
+		
+		long d=x;
+		System.out.println("Value of d: "+d);
+		
+		double e=x;
+		System.out.println("Value of e: "+e);
+		
+		
+		System.out.println("Explicit Type Casting");
+		
+		double z=77.8;
+		int y=(int)x;
+		System.out.println("Value of x: "+z);
+		System.out.println("Value of y: "+y);
+
+
+	}
+
+}
